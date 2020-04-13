c481e87601c6eade76e21b70b540bea18fb161f6
tiemfah <sivanat.sub@gmail.com>
2020-04-13 14:41:25 +0700

    typo

--- movie.pl
+++ movie.pl
@@ -1,7 +1,7 @@
 % Members : 6110545473 Chananchida Fuachai 
 %           6110545643 Sivanat Subpaisarn
 %           6110545660 Sirikon Songsaengthong 
-%           6110545673 Arisa Pangpeng
+%           6110545678 Arisa Pangpeng
 %           6110545783 Phanthakan Kiatphaisansophon
 
 /*
